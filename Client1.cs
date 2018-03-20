f2ac12f8fed1289eec4a0cdd85978131e38872f0
ankit chandra <ankitchandra1994@gmail.com>
2018-03-20 18:43:42 +0530

    Today Test program

---
+++ Client1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestClient
+{
+    class Client1
+    {
+       static void Main(string[] args)
+        {
+            log4net.Config.BasicConfigurator.Configure();
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(Client1));
+            ClientConnection();
+        }
+        public static void ClientConnection()
+        {   
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(Client1));
+            try
+            {
+                while (true)
+                {
+                    TcpClient tcpclnt = new TcpClient();
+                    log.Info("Connecting.....");
+
+                    tcpclnt.Connect("192.168.1.24", 8001);
+                    // use the ipaddress as in the server program
+
+                    log.Info("Connected");
+                    log.Info("Enter the string to be transmitted : ");
+
+                    String str = Console.ReadLine();
+                    Stream stm = tcpclnt.GetStream();
+
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    byte[] ba = asen.GetBytes(str);
+                    log.Info("Transmitting.....");
+
+                    stm.Write(ba, 0, ba.Length);
+
+                    byte[] bb = new byte[100];
+                    int k = stm.Read(bb, 0, 100);
+
+                    for (int i = 0; i < k; i++)
+                        Console.Write(Convert.ToChar(bb[i]));
+
+                    tcpclnt.Close();
+                }
+            }
+
+            catch (Exception e)
+            {
+                log.Error("Error..... " + e.StackTrace);
+            }
+
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleAppTest
+{
+    class Server
+    {
+        public static void Main(string[] args)
+        {
+            log4net.Config.BasicConfigurator.Configure();
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(Server));
+            ServerConnection();
+        }
+      public static void ServerConnection()
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(Server));
+
+         try
+            {
+                while (true)
+                {
+                    IPAddress ipAd = IPAddress.Parse("192.168.1.24");
+                    TcpListener myList = new TcpListener(ipAd, 8001);
+                    myList.Start();
+                    log.Info("The server is running at port 8001...");
+                    log.Info("The local End point is  :" +
+                    myList.LocalEndpoint);
+                    Console.WriteLine("Waiting for a connection.....");
+                    Socket s = myList.AcceptSocket();
+                    log.Info("Connection accepted from " + s.RemoteEndPoint);
+                    byte[] b = new byte[100];
+                    int k = s.Receive(b);
+                    log.Info("Recieved...");
+                    string Command = string.Empty;
+                    for (int i = 0; i < k; i++)
+                    {
+                        Command = Command + Convert.ToChar(b[i]);
+                    }
+                    Console.WriteLine(Command);
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    s.Send(asen.GetBytes("The string was recieved by the server."));
+                    Console.WriteLine("\nSent Acknowledgement");
+                    s.Close();
+                    myList.Stop();
+                }
+            }
+            catch (Exception e)
+            {
+             
+                log.Error("Error..... " + e.StackTrace);
+            }
+        }
+
+        
+    }
+}
